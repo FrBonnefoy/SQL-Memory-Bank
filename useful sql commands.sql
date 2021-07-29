e057d187b571648065abfbde504d254f248e68f9
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-07-29 17:42:27 +0200

    first

---
+++ useful sql commands.sql
@@ -0,0 +1,21 @@
+-- List all tables in database
+
+SET @database = 'name of db';
+select * from information_schema.tables
+where table_schema = @database
+order by table_name, ordinal_position
+
+
+--List all columns of database across tables
+
+SET @database = 'name of db';
+select * from information_schema.columns
+where table_schema = @database
+order by table_name, ordinal_position
+
+--List all columns of database across a specific table
+
+SET @database = 'name of db', @table='name of table';
+select * from information_schema.columns
+where table_schema = @database AND table_name=@table
+order by table_name, ordinal_position
