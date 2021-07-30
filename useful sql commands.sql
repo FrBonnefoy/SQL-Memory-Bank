75a516ac3cf91a0359a77472b9861495767f345d
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-07-30 16:16:33 +0200

    create wordcount function

--- useful sql commands.sql
+++ useful sql commands.sql
@@ -27,11 +27,37 @@ order by table_name, ordinal_position
 
 SET @database = 'mavenmovies';
 set @columnsearch = 'replacement_cost';
-SELECT 
+SELECT
     *
 FROM
     information_schema.columns
 WHERE
     table_schema = @database
         AND column_name = @columnsearch
-ORDER BY table_name , ordinal_position;	
+ORDER BY table_name , ordinal_position;
+
+-- 5.) Create wordcount function
+
+DELIMITER $$
+CREATE FUNCTION wordcount(str LONGTEXT)
+       RETURNS INT
+       DETERMINISTIC
+       SQL SECURITY INVOKER
+       NO SQL
+  BEGIN
+    DECLARE wordCnt, idx, maxIdx INT DEFAULT 0;
+    DECLARE currChar, prevChar BOOL DEFAULT 0;
+    SET maxIdx=char_length(str);
+    SET idx = 1;
+    WHILE idx <= maxIdx DO
+        SET currChar=SUBSTRING(str, idx, 1) RLIKE '[[:alnum:]]';
+        IF NOT prevChar AND currChar THEN
+            SET wordCnt=wordCnt+1;
+        END IF;
+        SET prevChar=currChar;
+        SET idx=idx+1;
+    END WHILE;
+    RETURN wordCnt;
+  END
+$$
+DELIMITER ;
