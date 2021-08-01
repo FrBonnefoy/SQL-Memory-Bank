b886578ed0b6c13288e613c770e945bb6661a095
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-08-01 20:44:58 +0200

    questions advanced 1

---
+++ tutorials/advanced.sql
@@ -0,0 +1,67 @@
+-- MySQL
+
+-- Channel conversion analysis
+
+/*
+Traffic source analysis is about understanding where your customers are
+coming from and which channels are driving the highest quality traffic
+
+• Analyzing search data and shifting budget
+towards the engines, campaigns or keywords
+driving the strongest conversion rates
+• Comparing user behavior patterns across
+traffic sources to inform creative and
+messaging strategy
+• Identifying opportunities to eliminate wasted
+spend or scale high-converting traffic
+*/
+
+/*1 We've been live for almost a month now and we’re
+starting to generate sales. Can you help me understand
+where the bulk of our website sessions are coming
+from, through yesterday?
+I’d like to see a breakdown by UTM source, campaign
+and referring domain if possible. Thanks! */
+
+
+/*2 Sounds like gsearch nonbrand is our major traffic source, but
+we need to understand if those sessions are driving sales.
+Could you please calculate the conversion rate (CVR) from
+session to order? Based on what we're paying for clicks,
+we’ll need a CVR of at least 4% to make the numbers work.
+If we're much lower, we’ll need to reduce bids. If we’re
+higher, we can increase bids to drive more volume.*/
+
+/*3 Based on your conversion rate analysis, we bid down
+gsearch nonbrand on 2012-04-15.
+Can you pull gsearch nonbrand trended session volume, by
+week, to see if the bid changes have caused volume to drop
+at all? */
+
+/*4 I was trying to use our site on my mobile device the other
+day, and the experience was not great.
+Could you pull conversion rates from session to order, by
+device type?
+If desktop performance is better than on mobile we may be
+able to bid up for desktop specifically to get more volume?*/
+
+/*5 After your device-level analysis of conversion rates, we
+realized desktop was doing well, so we bid our gsearch
+nonbrand desktop campaigns up on 2012-05-19.
+Could you pull weekly trends for both desktop and mobile
+so we can see the impact on volume?
+You can use 2012-04-15 until the bid change as a baseline*/
+
+-- Website performance analysis
+/*
+Website content analysis is about understanding which pages are seen the
+most by your users, to identify where to focus on improving your business
+
+• Finding the most-viewed pages that customers
+view on your site
+• Identifying the most common entry pages to
+your website – the first thing a user sees
+• For most-viewed pages and most common
+entry pages, understanding how those pages
+perform for your business objectives
+*/
